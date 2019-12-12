0b91e76cdd83b92b66a59b4eb0f2460459258cb6
Dun65333 <Dun65333@obu.edu>
2019-12-12 16:58:59 -0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner response = new Scanner(System.in);
+        System.out.println("Enter a place");
+        String place = response.nextLine();
+        System.out.println("Enter an animal");
+        String animal = response.nextLine();
+        System.out.println("Enter a height");
+        String height = response.nextLine();
+        System.out.println("Enter a food");
+        String food = response.nextLine();
     }
 }
